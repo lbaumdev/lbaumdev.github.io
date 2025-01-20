34cf3ffc03b5013be302041bb859ca44aeef98ad
Lukas Baum <50294438+lukasbaumweb@users.noreply.github.com>
2025-01-20 23:38:32 +0100

    fix validation which was a step behind

--- src/app.tsx
+++ src/app.tsx
@@ -14,15 +14,25 @@ const errors = {
   "end-date-in-past": "Enddatum darf nicht in der Vergangenheit liegen",
 };
 
+type ErrorCode = keyof typeof errors | null;
+type ValueState = {
+  choice: string;
+  amount: string;
+  endDate: Date | undefined;
+  errorCode: ErrorCode;
+};
+
 export function App() {
   const telegramSubmitButton = Telegram.WebApp.MainButton.setText("Zeitplan speichern").hide().disable();
   const in7Days = new Date(new Date().setDate(new Date().getDate() + 7));
 
-  const [choice, setChoice] = useState("0");
-  const [amount, setAmount] = useState("2");
-  const [endDate, setEndDate] = useState<Date | undefined>(in7Days);
+  const [values, setValues] = useState<ValueState>({
+    choice: "0",
+    amount: "2",
+    endDate: in7Days,
+    errorCode: null,
+  });
 
-  const [errorCode, setErrorCode] = useState<keyof typeof errors | null>(null);
   const options = [
     {
       label: "Jede Stunde",
@@ -47,41 +57,54 @@ export function App() {
   useEffect(() => {
     Telegram.WebApp.ready();
     telegramSubmitButton.onClick(function () {
-      if (!isValid(true)) return;
+      if (!isValid(values)) return;
 
       const data = JSON.stringify({
-        expression: getCronExpression(choice, amount),
-        text: getCronText(getCronExpression(choice, amount)),
+        expression: getCronExpression(values.choice, values.amount),
+        text: getCronText(getCronExpression(values.choice, values.amount)),
       });
       Telegram.WebApp.sendData(data);
       Telegram.WebApp.close();
     });
   });
 
-  const isValid = (showError = true) => {
-    if (endDate === undefined) {
-      showError && setErrorCode("missing-end-date");
+  const isValid = (_values: ValueState, showError = true) => {
+    const result: ValueState = {
+      ..._values,
+    };
+    if (_values.endDate === undefined) {
+      if (showError) {
+        result["errorCode"] = "missing-end-date";
+        setValues(result);
+      }
       return false;
     }
 
-    if (endDate.getTime() < new Date().getTime()) {
-      showError && setErrorCode("end-date-in-past");
+    if (_values.endDate.getTime() < new Date().getTime()) {
+      if (showError) {
+        result["errorCode"] = "end-date-in-past";
+        setValues(result);
+      }
       return false;
     }
 
-    showError && setErrorCode(null);
+    telegramSubmitButton.enable().show();
+
+    setValues({ ...result, errorCode: null });
+
     return true;
   };
-
-  if (errorCode === null && isValid()) {
-    telegramSubmitButton.enable().show();
-    console.log("enabled");
-  }
 
   return (
     <>
       <form>
-        <RadioGroup defaultValue="0" onValueChange={(c) => setChoice(c)} value={choice}>
+        <RadioGroup
+          defaultValue="0"
+          onValueChange={(c) => {
+            isValid({ ...values, choice: c });
+          }}
+          value={values.choice}
+        >
           <div className="flex items-center space-x-2">
             <RadioGroupItem value="0" id="c0" />
             <Label htmlFor="c0">Jede Stunde</Label>
@@ -92,10 +115,9 @@ export function App() {
             <Select
               name="amount-hour"
               onValueChange={(v) => {
-                setChoice("1");
-                setAmount(v);
+                isValid({ ...values, amount: v, choice: "1" });
               }}
-              value={amount}
+              value={values.amount}
             >
               <SelectTrigger className="w-[180px]">
                 <SelectValue placeholder="x" />
@@ -114,35 +136,40 @@ export function App() {
 
         <div className="mt-4 flex flex-col gap-4">
           Enddatum (Erforderlich)
-          <DatePicker date={endDate} setDate={(newDate) => setEndDate(newDate)} />
+          <DatePicker
+            date={values.endDate}
+            setDate={(newDate) => {
+              isValid({ ...values, endDate: newDate });
+            }}
+          />
         </div>
       </form>
-      {errorCode !== null && (
+      {values.errorCode !== null && (
         <div className={"mt-4"}>
           <Alert variant="destructive">
             <AlertCircle className="h-4 w-4" />
             <AlertTitle>Error</AlertTitle>
-            <AlertDescription>{errors[errorCode]}</AlertDescription>
+            <AlertDescription>{errors[values.errorCode]}</AlertDescription>
           </Alert>
         </div>
-      )}{" "}
+      )}
       {window.location.hash === "#debug" && (
         <div className={"mt-4"}>
           <pre>
             <code>
               {JSON.stringify(
                 {
-                  choice,
-                  amount,
-                  expression: getCronExpression(choice, amount),
-                  text: getCronText(getCronExpression(choice, amount)),
+                  choice: values.choice,
+                  amount: values.amount,
+                  expression: getCronExpression(values.choice, values.amount),
+                  text: getCronText(getCronExpression(values.choice, values.amount)),
                 },
                 null,
                 2
               )}
             </code>
           </pre>
-          <button onClick={() => console.log("Valid: " + isValid())}>Check</button>
+          <button onClick={() => console.log("Valid: " + isValid(values))}>Check</button>
         </div>
       )}
     </>
